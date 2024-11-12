b28957940915cabb0d6c62e7ffae3725e63f8037
saroj panthi <srzpanthi@gmail.com>
2024-11-12 18:50:44 +1100

    Implemented JWT authentication, bcrypt for password hashing, and mongoose-aggregate-paginate-v2 for pagination

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,7 @@
 import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 const userSchema = new Schema(
   {
     username: {
@@ -40,22 +43,50 @@ const userSchema = new Schema(
     password: {
       type: string,
       required: [true, "password is required"],
-     
-      
     },
 
     refreshToken: {
-        type: string 
+      type: string,
     },
 
-    watchHistory:[
-       {
+    watchHistory: [
+      {
         type: Schema.Types.ObjectId,
-        ref: "Video"
-       }
-    ]
+        ref: "Video",
+      },
+    ],
   },
   { timestamps: true }
 );
+
+userSchema.plugin(mongooseAggregatePaginate);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(this.password, password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    { _id: this._id, email: this.email, fullName: this.fullName },
+    process.env.ACCESS_TOKEN_EXPIRY,
+    { expiresIn: process.env.ACCEsS_TOKEN_EXPIRY }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
