baed504fa42b8e83905d82fdb20119f7dbc1de72
saroj panthi <srzpanthi@gmail.com>
2024-11-19 12:42:28 +1100

    added user controllers and user routes

--- src/app.js
+++ src/app.js
@@ -13,4 +13,8 @@ app.use(cookieParser())
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true}))
 app.use(express.static("public"))
+
+import userRouter from "./routes/user.routes.js"
+app.use("/api/v1/users", userRouter)
+
 export {app}
